65c12dfe43fdef29c56b73a07ed58f0484d8244c
Thomas Groshong <thomas.groshong@gmail.com>
2019-05-16 13:43:28 -0400

    Terraform fmt

--- terraform/website.tf
+++ terraform/website.tf
@@ -82,8 +82,8 @@ resource "aws_route53_record" "website" {
   type    = "A"
 
   alias {
-    name    = "${aws_cloudfront_distribution.website.domain_name}"
-    zone_id = "${aws_cloudfront_distribution.website.hosted_zone_id}"
+    name                   = "${aws_cloudfront_distribution.website.domain_name}"
+    zone_id                = "${aws_cloudfront_distribution.website.hosted_zone_id}"
     evaluate_target_health = true
   }
 }
